8983e2c433090d4c8268b6614b32a5f0747396a8
Piel-M <piel.mallet@gmail.com>
2025-03-26 16:41:22 +0100

    v3 finished

--- html/v3/script_v3.js
+++ html/v3/script_v3.js
@@ -48,7 +48,7 @@ $nextButton.on("click", () => {
 });
 
 // Event listener pour afficher les détails d'un pays
-$tableBody.on("click", "tr", function (event) {
+$tableBody.on("click", "tr",  function (event) {
     const countryIndex = $(this).data("country-index");
     const country = all_countries[countryIndex];
 
@@ -65,12 +65,18 @@ $tableBody.on("click", "tr", function (event) {
         <div class="details-header">
             <button class="close-details-button">X</button>
         </div>
-        <h2>${country.name}</h2>
-        <p><strong>Capitale:</strong> ${country.capital}</p>
-        <p><strong>Région:</strong> ${country.region}</p>
-        <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
-        <p><strong>Superficie:</strong> ${country.area} km²</p>
-        <img src="${country.flag}" alt="Drapeau de ${country.name}" class="img-drapeau">
+        <div class="details-content">
+            <div class="details-info">
+                <h2>${country.name}</h2>
+                <p><strong>Capitale:</strong> ${country.capital}</p>
+                <p><strong>Région:</strong> ${country.region}</p>
+                <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+                <p><strong>Superficie:</strong> ${country.area} km²</p>
+            </div>
+            <div class="details-flag">
+                <img src="${country.flag}" alt="Drapeau de ${country.name}" class="img-drapeau-details">
+            </div>
+        </div>
     `);
     
     // Ajouter un event listener pour fermer avec le bouton
@@ -79,6 +85,12 @@ $tableBody.on("click", "tr", function (event) {
     });
     
     $detailsZone.show();
+});
+
+$tableBody.on("click", ".img-drapeau-details", function () {
+    $flagModalImg.attr("src", $(this).attr("src"));
+    $flagModalImg.attr("alt", $(this).attr("alt"));
+    $flagModal.show();
 });
 
 // Event listener pour fermer la zone de détails
